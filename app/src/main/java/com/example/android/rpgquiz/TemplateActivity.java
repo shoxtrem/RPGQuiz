c46c3c672cd2b36bd5cfed57f0b0dc5f25722000
Fabien Boismoreau <fabien.boismoreau@gmail.com>
2018-02-02 13:14:15 +0100

    feat: Add the template Activity

---
+++ app/src/main/java/com/example/android/rpgquiz/TemplateActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.rpgquiz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TemplateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_template);
+    }
+}

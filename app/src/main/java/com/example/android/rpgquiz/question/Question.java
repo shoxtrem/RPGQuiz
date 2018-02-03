6b2f382a1ed800ea4035353513fcc024ca35af20
Fábio Gouveia <hpgroods@gmail.com>
2018-02-03 20:40:10 +0000

    feat : add getter methods
    Signed-off-by: Fábio Gouveia <hpgroods@gmail.com>

--- app/src/main/java/com/example/android/rpgquiz/question/Question.java
+++ app/src/main/java/com/example/android/rpgquiz/question/Question.java
@@ -4,22 +4,36 @@ package com.example.android.rpgquiz.question;
 
 public class Question {
 
-    //Instance variables
+    //Instance private members
     private String question; //Textual question
     private String[] possibleAnswer; // Array of possible answers
     private boolean[] possibleAnswerState; //Array of possible answers state
     private boolean passed; //Question passed state
 
 
-
-
-    /* API public mutator's (Setters) */
+    /**
+     * Returns a {@code String} representing this textual question.
+     *
+     * @return a <tt>String</tt> representing this textual question.
+     */
+    public String getQuestion() {
+        return question;
+    }
 
     /**
      * This method set up a new textual question
      **/
     public void setQuestion(String question) {
         this.question = question;
+    }
+
+    /**
+     * Returns a {@code boolean} representing the question passed state, true = passed / false = not passed.
+     *
+     * @return a <tt>boolean</tt> representing the question passed state.
+     */
+    public boolean passed() {
+        return passed;
     }
 
     /**

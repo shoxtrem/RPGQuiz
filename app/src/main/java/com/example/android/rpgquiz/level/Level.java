f145cf92faf5adf37e581025fd7b02a60423e7ca
Fábio Gouveia <hpgroods@gmail.com>
2018-02-03 23:26:08 +0000

    feat : add instance members
    Signed-off-by: Fábio Gouveia <hpgroods@gmail.com>

--- app/src/main/java/com/example/android/rpgquiz/level/Level.java
+++ app/src/main/java/com/example/android/rpgquiz/level/Level.java
@@ -1,8 +1,20 @@
 package com.example.android.rpgquiz.level;
 
+import com.example.android.rpgquiz.question.Question;
+
+import java.util.ArrayList;
+
 /**
  * Created by Fábio Gouveia on 03-02-2018.
  */
 
 public class Level {
+
+    //Instance members
+    private String levelName;// Level textual name
+    private ArrayList<Question> questions;// Questions list belonging to this level
+    private int score;// Level score
+    private boolean locked;// Level locked state
+
+
 }

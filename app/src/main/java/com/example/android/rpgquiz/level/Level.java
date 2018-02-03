1357b9e83d6b6f8930cdafaa463c3cba02889a9b
Fábio Gouveia <hpgroods@gmail.com>
2018-02-03 23:51:40 +0000

    feat : add getter methods
    Signed-off-by: Fábio Gouveia <hpgroods@gmail.com>

--- app/src/main/java/com/example/android/rpgquiz/level/Level.java
+++ app/src/main/java/com/example/android/rpgquiz/level/Level.java
@@ -22,10 +22,46 @@ public class Level {
     }
 
     /**
+     * Return a {@code String} representing this level textual name.
+     *
+     * @return a <tt>String</tt> representing this level textual name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Return a {@link ArrayList} of {@link Question} objects, this questions belonging to this level.
+     *
+     * @return a <tt>ArrayList</tt> of <tt>Question</tt> objects.
+     */
+    public ArrayList<Question> getQuestions() {
+        return questions;
+    }
+
+    /**
+     * Return a {@code int} representing this level actual score.
+     *
+     * @return a <tt>int</tt> representing this level score.
+     */
+    public int getScore() {
+        return score;
+    }
+
+    /**
      * This method set up this level score
      **/
     public void setScore(int score) {
         this.score = score;
+    }
+
+    /**
+     * Return a {@code boolean} representing if the level is locked, true = locked / false = unlocked.
+     *
+     * @return a <tt>boolean</tt> representing if the level is locked.
+     */
+    public boolean isLocked() {
+        return locked;
     }
 
     /**
@@ -35,5 +71,22 @@ public class Level {
         this.locked = locked;
     }
 
-
+    /**
+     * Return if the level was passed, {@code true} = passed / {@code false} = not passed
+     *
+     * @return a <tt>boolean</tt> representing the level passed state.
+     */
+    public boolean passed() {
+        //Initialize variable to true and let the iterator do the rest
+        boolean passed = true;
+        //Iterate over questions array
+        for (Question question : questions) {
+            //We find a not passed question...Break immediately and return false
+            if (!question.passed()) {
+                passed = false;
+                break;
+            }
+        }
+        return passed;
+    }
 }
